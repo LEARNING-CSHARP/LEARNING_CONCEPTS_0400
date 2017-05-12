e698522e60493da1e9712705d02f81d6d3b2c8eb
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-05-12 13:11:36 +0430

    Add project files.

---
+++ LEARNING_CONCEPTS/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace LEARNING_CONCEPTS
+{
+	public class Person
+	{
+		public int Age;
+		public string FullName;
+
+		public void ShowInfo()
+		{
+			// Solution (1)
+			//string strMessage =
+			//	string.Format("I'm {0} and {1} years old.", FullName, Age);
+
+			//System.Console.WriteLine(strMessage);
+
+			// Solution (2)
+			System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+		}
+	}
+}
---
+++ LEARNING_CONCEPTS/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace LEARNING_CONCEPTS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// **********
+			//Person P1;
+
+			//P1.Age = 20; // Error!
+			// **********
+
+			// **********
+			Person P1;
+			P1 = new Person();
+
+			P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.Age = 20;
+			//P1.FullName = "Ali Reza Alavi";
+
+			//P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.Age = 20;
+			//P1.FullName = "Ali Reza Alavi";
+
+			//P1.ShowInfo();
+
+			//Person P2 = new Person();
+
+			//P2.Age = 30;
+			//P2.FullName = "Sara Ahmadi";
+
+			//P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.Age = 20;
+			//P1.FullName = "Ali Reza Alavi";
+
+			//P1.ShowInfo();
+
+			//Person P2 = new Person();
+
+			//P2 = P1;
+
+			//P2.Age = 30;
+
+			//P1.ShowInfo();
+			// **********
+
+			// **********
+			//Person P1 = new Person();
+
+			//P1.Age = 20;
+			//P1.FullName = "Ali Reza Alavi";
+
+			//P1.ShowInfo();
+
+			//Person P2;
+
+			//P2 = P1;
+
+			//P2.Age = 30;
+
+			//P1.ShowInfo();
+			// **********
+
+			System.Console.Write("Press [ENTER] To Exit...");
+			System.Console.ReadLine();
+		}
+	}
+}
