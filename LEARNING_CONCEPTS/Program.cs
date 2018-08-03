8b334059c7ca7f1d69f1ae409da9e5d3723ae962
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-08-03 12:06:06 +0430

    Making some changes!

--- LEARNING_CONCEPTS/Program.cs
+++ LEARNING_CONCEPTS/Program.cs
@@ -5,68 +5,36 @@
 		static void Main(string[] args)
 		{
 			// **********
-			//Person p1;
+			//Person p;
 
-			//p1.Age = 20; // Compile Error!
+			//p.Age = 20; // Compile Error!
+			// **********
+
+			// **********
+			//Person p;
+			//p = new Person();
+
+			//p.ShowInfo();
+			// **********
+
+			// **********
+			//Person p = new Person();
+
+			//p.ShowInfo();
+			// **********
+
+			// **********
+			//Person p = new Person();
+
+			//p.Age = 20;
+			//p.FullName = "Ali Reza Alavi";
+
+			//p.ShowInfo();
 			// **********
 
 			// **********
 			//Person p1;
 			//p1 = new Person();
-
-			//p1.ShowInfo();
-			// **********
-
-			// **********
-			//Person p1 = new Person();
-
-			//p1.ShowInfo();
-			// **********
-
-			// **********
-			//Person p1 = new Person();
-
-			//p1.Age = 20;
-			//p1.FullName = "Ali Reza Alavi";
-
-			//p1.ShowInfo();
-			// **********
-
-			// **********
-			//Person p1 = new Person();
-
-			//p1.Age = 20;
-			//p1.FullName = "Ali Reza Alavi";
-
-			//p1.ShowInfo();
-
-			//Person p2 = new Person();
-
-			//p2.Age = 30;
-			//P2.FullName = "Sara Ahmadi";
-
-			//p1.ShowInfo();
-			// **********
-
-			// **********
-			//Person p1 = new Person();
-
-			//p1.Age = 20;
-			//p1.FullName = "Ali Reza Alavi";
-
-			//p1.ShowInfo();
-
-			//Person p2 = new Person();
-
-			//p2 = p1;
-
-			//p2.Age = 30;
-
-			//p1.ShowInfo();
-			// **********
-
-			// **********
-			//Person p1 = new Person();
 
 			//p1.Age = 20;
 			//p1.FullName = "Ali Reza Alavi";
@@ -74,6 +42,25 @@
 			//p1.ShowInfo();
 
 			//Person p2;
+			//p2 = new Person();
+
+			//p2.Age = 30;
+			//P2.FullName = "Sara Ahmadi";
+
+			//p2.ShowInfo();
+			// **********
+
+			// **********
+			//Person p1;
+			//p1 = new Person();
+
+			//p1.Age = 20;
+			//p1.FullName = "Ali Reza Alavi";
+
+			//p1.ShowInfo();
+
+			//Person p2;
+			//p2 = new Person();
 
 			//p2 = p1;
 
@@ -83,18 +70,51 @@
 			// **********
 
 			// **********
-			//Person p1 = new Person();
+			//Person p1;
+			//p1 = new Person();
 
 			//p1.Age = 20;
 			//p1.FullName = "Ali Reza Alavi";
 
 			//p1.ShowInfo();
 
-			//Person p2 = p1;
+			//Person p2;
+			//p2 = p1;
 
 			//p2.Age = 30;
 
 			//p1.ShowInfo();
+			// **********
+
+			// **********
+			//Person p1;
+			//p1 = new Person();
+
+			//p1.Age = 20;
+			//p1.FullName = "Ali Reza Alavi";
+
+			//p1.ShowInfo();
+
+			//Person p2;
+			//p2 = p1;
+
+			//p2.Age = 30;
+
+			//p1.ShowInfo();
+
+			//p2 = null;
+
+			//p2.Age = 40; // Runtime Error!
+			// **********
+
+			// **********
+			// سوال: در نمونه کد ذیل چند شیء داریم؟
+
+			//person p1 = new Person();
+
+			//person p2 = p1;
+			//person p3 = p1;
+			//person p4 = p1;
 			// **********
 
 			System.Console.Write("Press [ENTER] To Exit...");
