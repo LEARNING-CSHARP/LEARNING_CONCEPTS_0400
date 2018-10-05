993405aaba42b25a3c4b01e1532785086b8fbe66
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-10-05 16:41:57 +0330

    Making some changes!

---
+++ LEARNING_CONCEPTS/Human.cs
@@ -0,0 +1,13 @@
+﻿namespace LEARNING_CONCEPTS
+{
+	public class Human
+	{
+		public int Age;
+		public string FullName;
+
+		public void ShowInfo()
+		{
+			System.Console.WriteLine($"I'm { FullName } and { Age } years old.");
+		}
+	}
+}
